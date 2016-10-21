87a2503f2d2c3bc47db2621545db318643f879fb
Shivan.Naidoo <Shivan.Naidoo@interfront.co.za>
2016-10-21 16:22:14 +0200

    wip

--- src/main/java/com/example/UiApplication.java
+++ src/main/java/com/example/UiApplication.java
@@ -2,9 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -21,8 +27,29 @@ public class UiApplication {
 		return model;
 	}
 
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
 	}
+
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                    .anyRequest().authenticated();
+        }
+    }
+
 
 }
